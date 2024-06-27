0cec96e7fe258ce1e6c73cd088a31ff948f4d3b2
Khue Vo <vo005642@stthomas.edu>
2024-06-27 09:45:42 +0700

    step 2: Create an Entry-Point Script

--- clitodo/__main__.py
+++ clitodo/__main__.py
@@ -1,1 +1,11 @@
- 
+"""CLI To-Do entry point script"""
+# clitodo/__main__.py
+
+from clitodo import cli, __app_name__
+
+def main():
+    cli.app(prog_name=__app_name__) #Call the Typer app, passing the application's name to the prog_name argument,
+                                    # ensure that the users get the correct app name when running the --help option on their command line
+
+if __name__ == "__main__":
+    main()
--- clitodo/cli.py
+++ clitodo/cli.py
@@ -11,17 +11,23 @@ app = typer.Typer() #Create an explicit Typer application
 
 def _version_callback(value: bool) -> None:
     if value:
-        typer.echo(f"{__app_name__} v{__version__}")
-        raise typer.Exit()
+        typer.echo(f"{__app_name__} v{__version__}") #Prints the application name and version
+        raise typer.Exit() #Raises an exception to exit the application
 
-@app.callback()
+@app.callback() #Define main() as a Typer callback using the @app.callback() decorator
 def main(
+        #Define version, which is of type Optional[bool]
         version: Optional[bool] = typer.Option(
+            #The first argument to the initializer of Option
             None,
+            #Set the command-line names for the version option
             "--version",
             "-v",
+            #Provides a help message for the version option
             help="Show the application' version and exit.",
+            #Attaches a callbcak function to the version option, running the option automatically calls the function
             callback=_version_callback,
+            #Tells Typer that the version command-line option has precedence over commands in the current application
             is_eager=True,
         )
 ) -> None:
