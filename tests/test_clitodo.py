568221d35c3995186589faa68b06e479cfb32850
Khue Vo <vo005642@stthomas.edu>
2024-06-27 09:57:59 +0700

    step 2: Set Up Initial CLI Tests with Pytest

--- tests/test_clitodo.py
+++ tests/test_clitodo.py
@@ -1,1 +1,14 @@
- 
+# tests/test_clitodo.py
+
+from typer.testing import CliRunner
+
+from clitodo import __app_name__, __version__, cli
+
+runner = CliRunner()
+
+def test_version(): #Define first unit test for testing the application's version
+    result = runner.invoke(cli.app, ["__version"]) #Run the application with the __version option,
+                                                        #stop the result of this call in result
+    assert result.exit_code == 0 #Assert that the application's exit code is equal to 0
+                                 #to check that the application ran successfully
+    assert f"{__app_name__} v{__version__}\n" in result.stdout #Assert that the application's version is present with the standard output,
