2367fd3007a8e7b984627bca5f84d293de68b98c
Khue Vo <vo005642@stthomas.edu>
2024-06-27 09:08:18 +0700

    step 2

--- clitodo/__init__.py
+++ clitodo/__init__.py
@@ -1,6 +1,7 @@
 """Top-level package for CLI To-Do"""
 # clitodo/__init__.py
 
+#Two module level names to hold the application's name and version
 __app_name__ = "clitodo"
 __version__ = "0.1.0"
 
@@ -12,7 +13,8 @@ __version__ = "0.1.0"
      DB_WRITE_ERROR,
      JSON_ERROR,
      ID_ERROR,
- ) = range(7)
+ ) = range(7) # A series of return and error codes
+
 
 ERRORS = {
      DIR_ERROR: "config directory error",
@@ -20,4 +22,4 @@ ERRORS = {
      DB_READ_ERROR: "database read error",
      DB_WRITE_ERROR: "database write error",
      ID_ERROR: "to-do id error",
- }
+ } #A dictionary that maps error codes to human-readable error messages
--- clitodo/cli.py
+++ clitodo/cli.py
@@ -1,1 +1,28 @@
- 
+"""This module provides the CLI To-Do List"""
+# clitodo/cli.py
+
+from typing import Optional
+
+import typer
+
+from clitodo import __app_name__, __version__
+
+app = typer.Typer() #Create an explicit Typer application
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+
+@app.callback()
+def main(
+        version: Optional[bool] = typer.Option(
+            None,
+            "--version",
+            "-v",
+            help="Show the application' version and exit.",
+            callback=_version_callback,
+            is_eager=True,
+        )
+) -> None:
+    return
