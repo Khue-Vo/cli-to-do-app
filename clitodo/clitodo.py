9f8033280bd663e5ad3b7a5625e975497ddc810e
Khue Vo <vo005642@stthomas.edu>
2024-06-27 14:38:54 +0700

    step 4: Set Up the To-Do App Back End

--- clitodo/clitodo.py
+++ clitodo/clitodo.py
@@ -1,1 +1,8 @@
- 
+"""This module provides the CLI To-Do model-controller."""
+# clitodo/clitodo.py
+
+from typing import  Any, Dict, NamedTuple
+
+class CurrentToDo(NamedTuple): #Create a subclass of typing.NamedTuple with two fields
+    todo: Dict[str, Any]
+    error: int
