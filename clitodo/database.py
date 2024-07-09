b63b7c6fd058d76c8cbdb13cef720775d7a59b3a
Khue Vo <vo005642@stthomas.edu>
2024-07-09 16:49:21 +0700

    #12 SQL3 - Connect to Database

--- clitodo/database.py
+++ clitodo/database.py
@@ -9,7 +9,7 @@ from typing import Any, Dict, List, NamedTuple
 from clitodo import DB_READ_ERROR, DB_WRITE_ERROR, SQL3_ERROR, SUCCESS
 
 DEFAULT_DB_FILE_PATH = Path.home().joinpath(
-    "." +Path.home().stem + "_todo.json"
+    "." +Path.home().stem + "_todo.db"
 ) # Create a holder for the default database file path
   # The application will use this path if the user doesn't provide a custom one
 
@@ -24,33 +24,33 @@ def init_database(db_path: Path) -> int:
     """Create the to-do database."""
     try:
         conn = sqlite3.connect(db_path)
-
+        conn.close()
         return SUCCESS
-    except OSError:
+    except sqlite3.Error as e:
         return DB_WRITE_ERROR
 
-class DBResponse(NamedTuple):
-    todo_list: List[Dict[str, Any]] #The to-do list users will write and read from the database
-    error: int #An integer number representing a return code related to the current database operation
-
-class DatabaseHandler: #Allow users to read and write data to the to-do database using the json module from the standard library
-    def __init__(self, db_path: Path) -> None: #Define class initializer
-        self._db_path = db_path
-
-    def read_todos(self) -> DBResponse: #This method reads the to-do list from tha database and deserializes it
-        try: #Catch any errors that occur while users are opening the database
-            with self._db_path.open("r") as db:
-                try:
-                    return DBResponse(json.load(db), SUCCESS)
-                except json.JSONDecodeError: #Catch wrong JSON format
-                    return DBResponse([], DB_READ_ERROR)
-        except OSError: #Catch file IO problems
-            return DBResponse([], DB_READ_ERROR)
-
-    def write_todos(self, todo_list: List[Dict[str, Any]]) -> DBResponse: #Take a list of to-do dictionaries and write it to the database
-        try: #Catch any errors that occur while users are opening the database
-            with self._db_path.open("w") as db: #Open the database for writing
-                json.dump(todo_list, db, indent=4) #Dump the to-do list as a JSON payload into the database
-            return DBResponse(todo_list, SUCCESS)
-        except OSError: #Catch file IO problems
-            return DBResponse(todo_list, DB_WRITE_ERROR)
+# class DBResponse(NamedTuple):
+#     todo_list: List[Dict[str, Any]] #The to-do list users will write and read from the database
+#     error: int #An integer number representing a return code related to the current database operation
+#
+# class DatabaseHandler: #Allow users to read and write data to the to-do database using the json module from the standard library
+#     def __init__(self, db_path: Path) -> None: #Define class initializer
+#         self._db_path = db_path
+#
+#     def read_todos(self) -> DBResponse: #This method reads the to-do list from tha database and deserializes it
+#         try: #Catch any errors that occur while users are opening the database
+#             with self._db_path.open("r") as db:
+#                 try:
+#                     return DBResponse(json.load(db), SUCCESS)
+#                 except json.JSONDecodeError: #Catch wrong JSON format
+#                     return DBResponse([], DB_READ_ERROR)
+#         except OSError: #Catch file IO problems
+#             return DBResponse([], DB_READ_ERROR)
+#
+#     def write_todos(self, todo_list: List[Dict[str, Any]]) -> DBResponse: #Take a list of to-do dictionaries and write it to the database
+#         try: #Catch any errors that occur while users are opening the database
+#             with self._db_path.open("w") as db: #Open the database for writing
+#                 json.dump(todo_list, db, indent=4) #Dump the to-do list as a JSON payload into the database
+#             return DBResponse(todo_list, SUCCESS)
+#         except OSError: #Catch file IO problems
+#             return DBResponse(todo_list, DB_WRITE_ERROR)
